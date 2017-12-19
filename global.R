de35bd36d5e569096a18bdbf74595146b8382006
laderast <roshioshi@gmail.com>
2017-12-19 09:28:23 -0800

    first commit

---
+++ global.R
@@ -0,0 +1,39 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(data.table)
+library(tidyverse)
+
+###Put Data processing steps here
+#load dataset, loading strings as categorical data
+#dataset <- fread("data/genoData.csv",stringsAsFactors = TRUE)
+library(NHANES)
+data(NHANES)
+dataset <- NHANES
+
+#dataset <- dataset %>% filter(numAge > 50 & gender =="F") %>%
+#  select(cvd, htn) 
+
+#Uncomment this line to load the genetic data
+#dataset <- fread("data/genoData.csv",stringsAsFactors = TRUE)
+#make this variable a character
+#dataset$patientID <- as.character(dataset$patientID)
+#make this variable an ordered categorical variable
+#dataset$age <- ordered(dataset$age)
+
+
+##Don't modify anything below here, or app won't work properly.
+##get the variable types
+varClass <- sapply(dataset, class)
+varClass <- lapply(varClass, function(x){if(length(x)>1)x <- x[2]; return(x)})
+
+#separate the variables into each type
+categoricalVars <- names(varClass[varClass %in% c("factor","ordered")])
+numericVars <- names(varClass[varClass %in% c("numeric", "integer")])
+
+dataset <- data.table(dataset)
+
+##maybe add a who's missing?
+
+##add a list of outcomes
+
---
+++ readme.md
@@ -0,0 +1,16 @@
+## Data Diving into NHANES
+
+A Shiny Dashboard for exploring the NHANES dataset. RStudio and R is required to use this on your computer.
+
+You will need the following packages: `tidyverse`, `shiny`, `NHANES`
+
+```
+install.packages(c("tidyverse", "shiny", "NHANES"))
+```
+
+To run the dashboard, clone this repo, open the `DataDiveNHANES.Rproj` file. Then do the following:
+
+```
+library(shiny)
+runApp()
+```
